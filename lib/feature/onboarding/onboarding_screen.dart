57fc8bb92e60ca7a329ecec8a823d6783d7db4ec
Youssef Emad <youssefemad347@gmail.com>
2026-05-06 05:25:23 +0300

    onboardingScreen

---
+++ lib/feature/onboarding/onboarding_screen.dart
@@ -0,0 +1,71 @@
+import 'package:eqraa_book_store/core/widgets/main_button.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          // fit: StackFit.expand,
+          children: [
+            Center(
+              child: SizedBox(
+                height: size.height * .9,
+                child: Image.asset(
+                  "assets/onBoardingPhoto.png",
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.white,
+                width: double.infinity,
+                height: size.height * 0.35,
+                child: Column(
+                  children: [
+                    Text(
+                      textAlign: TextAlign.center,
+                      "Your personal library in your pocket",
+                      style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: size.height * .01),
+                    Text(
+                      textAlign: TextAlign.center,
+                      "It is a long established fact that a reader will be distracted by the readable content.",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w100,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.02),
+                    MainButton(
+                      title: "Next",
+                      isEnabled: true,
+                      onPressedFunction: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
