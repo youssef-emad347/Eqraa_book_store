16c616f8bf888e79abe273e7950afeea17967079
Youssef Emad <youssefemad347@gmail.com>
2026-05-09 22:57:45 +0300

    add global border

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:eqraa_book_store/feature/authentication/forget_password/forget_password.dart';
+import 'package:eqraa_book_store/core/constants/constants.dart';
 import 'package:eqraa_book_store/feature/authentication/login/login_screen.dart';
 import 'package:eqraa_book_store/feature/authentication/signup/signup_screen.dart';
+import 'package:eqraa_book_store/feature/home/home_screen.dart';
 import 'package:eqraa_book_store/feature/onboarding/onboarding_screen.dart';
-import 'package:eqraa_book_store/feature/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,12 +13,30 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.grey, width: 1.5),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade300, width: 1.5),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Constants.mainColor,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: HomeScreen(),
       routes: {
         "/onboarding": (context) => OnboardingScreen(),
         "/login": (context) => LoginScreen(),
